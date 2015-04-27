1b4bf22458aafc133a28c58bad2b3237a935ea9c
t33soccer <t33soccer@gmail.com>
2015-04-27 11:04:08 -0500

    Mid-Commit

--- makeBlankGraph.cpp
+++ makeBlankGraph.cpp
@@ -1,25 +1,18 @@
 using namespace std;
-#include "grmat.h"
-// #include "grlist.h"
+// #include "grmat.h"
+#include "grlist.h"
 #include "makeBlankGraph.h"
 #include <fstream>
 int main() {
 	int graphsize;
 	
-	graphsize = 10;
-	
+	graphsize = 750;
 	ofstream fout;
-	
 	fout.open("graph.txt");
-	
-	Graphm MyGraph(graphsize);
-	
+	Graphl MyGraph(graphsize);
 	cout << MyGraph.e() << endl;
-	
 	makeBlankGraph(MyGraph,graphsize,true);
-	
 	cout << MyGraph.e() << endl;
-	
 	MyGraph.serializegph(fout);
 	
 	
