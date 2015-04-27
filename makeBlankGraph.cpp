b4b0e84733344815727e0c014815ab4d4f26fe24
t33soccer <t33soccer@gmail.com>
2015-04-27 13:15:01 -0500

    More tests run

--- makeBlankGraph.cpp
+++ makeBlankGraph.cpp
@@ -1,6 +1,6 @@
 using namespace std;
-// #include "grmat.h"
-#include "grlist.h"
+#include "grmat.h"
+// #include "grlist.h"
 #include "makeBlankGraph.h"
 #include <fstream>
 int main() {
@@ -9,7 +9,7 @@ int main() {
 	graphsize = 750;
 	ofstream fout;
 	fout.open("graph.txt");
-	Graphl MyGraph(graphsize);
+	Graphm MyGraph(graphsize);
 	cout << MyGraph.e() << endl;
 	makeBlankGraph(MyGraph,graphsize,true);
 	cout << MyGraph.e() << endl;
